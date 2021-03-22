ae8c8f4c7baf83a109efc70fd7c9862970e4432b
Ralf Lang <lang@b1-systems.de>
2021-03-22 06:49:56 +0000

    Backward compatibility with horde5 rpc. URL Rewriting is not sufficient.

---
+++ web/rpc/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Compatibility with Horde 5 expectations
+ *
+ * rpc url's like dav paths are generated from the horde webroot.
+ * But horde webroot is no longer in the root dir of a horde installation.
+ * Caldav does not like simple URL forwarding from outside what it thinks is webroot.
+ *
+ */
+$_SERVER['REQUEST_URI'] = substr_replace($_SERVER['REQUEST_URI'], '/horde/rpc/', -8, 5);
+$_SERVER['SCRIPT_FILENAME'] = substr_replace($_SERVER['SCRIPT_FILENAME'], '/horde', -10, 0);
+$_SERVER['SCRIPT_NAME'] = substr_replace($_SERVER['SCRIPT_NAME'], '/horde', -10, 0);
+$_SERVER['PHP_SELF'] = substr_replace($_SERVER['PHP_SELF'], '/horde', -10, 0);
+
+require __DIR__ . '/../horde/rpc.php';
